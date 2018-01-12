a0371a081cfbeaae3a64b046d5929d25b3d23f4d
Wagner Almeida <wdmeida@gmail.com>
2018-01-12 20:22:37 -0200

    Created TwdServiceProvider

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { TwdServiceProvider } from '../../providers/twd-service/twd-service';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,21 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  public obj: any;
+  public result: any;
 
+  constructor(
+    public navCtrl: NavController,
+    public twdService: TwdServiceProvider
+  ) {
+    this.getAll();
   }
 
+  public getAll() {
+    this.twdService.load()
+      .then(data => {
+        this.obj = data;
+        this.result = this.obj._embedded.episodes;
+      });
+  }
 }
---
+++ src/providers/twd-service/twd-service.ts
@@ -0,0 +1,38 @@
+import { Http } from '@angular/http'
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import 'rxjs/add/operator/map';
+
+/*
+  Generated class for the TwdServiceProvider provider.
+
+  See https://angular.io/guide/dependency-injection for more info on providers
+  and Angular DI.
+*/
+@Injectable()
+export class TwdServiceProvider {
+  public data: any;
+
+  constructor(public http: Http) {
+    console.log('Hello TwdServiceProvider Provider');
+  }
+
+  public load() {
+    if (this.data) {
+      return Promise.resolve(this.data);
+    }
+
+    return new Promise(resolve => {
+      this.http
+        .get(
+          `https://api.tvmaze.com/singlesearch/shows?q=the-walking-dead&embed=episodes`
+        )
+        .map(res => res.json())
+        .subscribe(data => {
+          this.data = data;
+          resolve(this.data);
+        });
+    });
+  }
+
+}
